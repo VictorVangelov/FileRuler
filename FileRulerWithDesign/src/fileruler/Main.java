dea68d21814f1167cd3eaa85164fc6e7d4cd1d37
svilen <svilenkomitov@gmail.com>
2015-05-28 22:31:09 +0300

    change

--- FileRulerWithDesign/src/fileruler/Main.java
+++ FileRulerWithDesign/src/fileruler/Main.java
@@ -1,11 +1,7 @@
 package fileruler;
 
 import java.io.IOException;
-import java.util.List;
 
-import fileruler.dao.DAOManager;
-import fileruler.dao.MovieDAO;
-import fileruler.model.Movie;
 import fileruler.view.BaseController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
