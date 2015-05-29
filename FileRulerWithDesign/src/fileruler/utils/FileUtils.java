addd075e11810da8fad1c08e796104b6817128ec
svilen <svilenkomitov@gmail.com>
2015-05-29 12:03:29 +0300

    add

--- FileRulerWithDesign/src/fileruler/utils/FileUtils.java
+++ FileRulerWithDesign/src/fileruler/utils/FileUtils.java
@@ -1,8 +1,14 @@
 package fileruler.utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 
 import org.apache.commons.io.IOUtils;
 
@@ -20,13 +26,30 @@ public class FileUtils {
 	
 	public static String getFileContent(String filePath) {
 		
-		String target = new String();
+		String content = new String();
 		try(FileInputStream stream = new FileInputStream(new File(filePath))) {
-			target = IOUtils.toString(stream, "UTF-8");
+			content = IOUtils.toString(stream, "UTF-8");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return target;
+		return content;
+	}
+	
+	public static void copyFromTo(String from, String to) {
+		
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+	              new FileOutputStream(to), "utf-8"))) {
+	   writer.write(getFileContent(from));
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 	
 }
