f738f1f24360e388648b1e37dd7ccee361c21d9a
svilen <svilenkomitov@gmail.com>
2015-05-29 11:34:46 +0300

    commit

--- FileRulerWithDesign/src/fileruler/utils/FileUtils.java
+++ FileRulerWithDesign/src/fileruler/utils/FileUtils.java
@@ -1,5 +1,11 @@
 package fileruler.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
 public class FileUtils {
 
 	public static String encode(String decoded) {
@@ -10,5 +16,16 @@ public class FileUtils {
 	public static String decode(String encoded) {
 		
 		return encoded.replace("-", "\\\\").replace('_', ':');
+	}
+	
+	public static String getFileContent(String filePath) {
+		
+		String target = new String();
+		try(FileInputStream stream = new FileInputStream(new File(filePath))) {
+			target = IOUtils.toString(stream, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return target;
 	}
 }
--- FileRulerWithDesign/src/fileruler/utils/Main.java
+++ FileRulerWithDesign/src/fileruler/utils/Main.java
@@ -79,5 +79,6 @@ private static String PERSISTENCE_UNIT_NAME = "movies";
 		
 		System.out.println(FileUtils.encode("C:\\ProgramFiles\\SomeDocs\\myInfo.txt"));
 		System.out.println(FileUtils.decode(FileUtils.encode("C:\\ProgramFiles\\SomeDocs\\myInfo.txt")));
+		System.out.println(FileUtils.getFileContent("/home/svilen/Desktop/fiile.config.xml"));
 	}
 }
